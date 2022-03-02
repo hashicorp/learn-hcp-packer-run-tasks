c5282c5bcb7cbfdf121f7843351c94cebc1dced3
Tu Nguyen <im2nguyen@gmail.com>
2022-03-02 02:23:44 -0800

    Add HCP provider back to resource validation

--- tf-resource-validation/terraform.tf
+++ tf-resource-validation/terraform.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    hcp = {
+      source  = "hashicorp/hcp"
+      version = "0.22.0"
+    }
     aws = {
       source  = "hashicorp/aws"
       version = "4.2.0"
